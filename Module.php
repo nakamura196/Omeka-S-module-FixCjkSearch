6cb1e77f91640e21e849df02dce19e4d1b0e9c63
Satoru <you@example.com>
2022-12-23 16:33:22 +0900

    feat: add script

--- Module.php
+++ Module.php
@@ -2,6 +2,12 @@
 
 namespace FixCjkSearch;
 
+if (!class_exists(\Generic\AbstractModule::class)) {
+    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
+        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
+        : __DIR__ . '/src/Generic/AbstractModule.php';
+}
+
 use Generic\AbstractModule;
 
 class Module extends AbstractModule
