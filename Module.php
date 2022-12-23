a3f085b734ee97850563a290171917c104363bce
Satoru <you@example.com>
2022-12-23 15:02:03 +0900

    feat: initial commit

---
+++ Module.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace FixCjkSearch;
+
+use Generic\AbstractModule;
+
+class Module extends AbstractModule
+{
+    const NAMESPACE = __NAMESPACE__;
+}
---
+++ config/module.config.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+namespace FixCjkSearch;
+
+return [
+    'view_manager' => [
+        'template_path_stack' => [
+            dirname(__DIR__) . '/view',
+        ],
+    ]
+];
